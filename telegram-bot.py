59a9303a629c1ae9e482e8e7a33926c355769914
christian-fr <karl.wilhelm@posteo.de>
2020-01-01 01:35:57 +0100

    initial commit

---
+++ telegram-bot.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater, CommandHandler
+
+
+
+def hello(update, context):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+updater = Updater('TOKEN', use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+
+
+updater.start_polling()
+updater.idle()
