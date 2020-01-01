f98099b91825186774fcfa01aa1b85b712d68af7
christian-fr <karl.wilhelm@posteo.de>
2020-01-01 01:54:18 +0100

    version 2

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,17 +1,37 @@
-from telegram.ext import Updater, CommandHandler
+with open(r'token', 'r') as f:
+    token = f.readline()
+    # print(token)
+
+import os
+from telegram.ext import Updater
+from telegram.ext import filters
 
 
+updater = Updater(token=token, use_context=True)
 
-def hello(update, context):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+dispatcher = updater.dispatcher
 
-
-updater = Updater('TOKEN', use_context=True)
-
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start, filters.Filters.user(username='@pythonista123'))
+dispatcher.add_handler(start_handler)
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    myCmd = 'youtube-dl -x ' + update.message.text
+    os.system(myCmd)
+    myCmd = 'mv *.opus /media/usb-hdd/Musik/_temp && mpc update _temp'
+    os.system(myCmd)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo, Filters.user(username='@pythonista123'))
+
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
-updater.idle()
