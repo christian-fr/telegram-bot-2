96b73023ad4866a0a76b6189fb1498fca9a21ffc
christian-fr <cfriedrich@posteo.de>
2021-01-03 19:26:25 +0100

    several files modified

--- pyppeteerHeadlessScreenshotter/pyppeteer_screenshotter.py
+++ pyppeteerHeadlessScreenshotter/pyppeteer_screenshotter.py
@@ -3,6 +3,7 @@ import asyncio
 from pyppeteer import launch
 import os
 import logging
+from pathlib import Path
 
 # Get the logger specified in the file
 logger = logging.getLogger(__name__)
@@ -10,18 +11,23 @@ logger = logging.getLogger(__name__)
 logger.info('Starting up.')
 
 
-async def main():
+async def pyppeteer_main(url_str, resolution_dict, output_file):
+    assert isinstance(url_str, str)
+    assert isinstance(resolution_dict, dict)
+    assert isinstance(output_file, str) or isinstance(output_file, Path)
     browser = await launch(headless=True)
     page = await browser.newPage()
-    await page.setViewport({'width': 1600, 'height': 900})
+    await page.setViewport(resolution_dict)
     # await page.setViewport({'width': 1920, 'height': 1080})
 
-    template_file = os.path.abspath(os.path.join(os.path.dirname(__file__), './template.html'))
+    template_file = url_str
     print(template_file)
     logger.info('template_file: ' + str(template_file))
     await page.goto('file://'+template_file)
 
-    await page.screenshot({'path': 'example.png'})
+    await page.screenshot({'path': str(output_file)})
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+# asyncio.get_event_loop().run_until_complete(pyppeteer_main(url_str=os.path.abspath(os.path.join(os.path.dirname(__file__), './template.html')), resolution_dict={'width': 1600, 'height': 900}, output_file='example.png'))
+
+
