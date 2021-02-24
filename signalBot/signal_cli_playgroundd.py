6e7769404f15b0545773522c463a07a05c31a68c
christian <123>
2021-02-24 19:49:27 +0100

    working first draft with OverlayImageRenderer.py

--- signalBot/signal_cli_playgroundd.py
+++ signalBot/signal_cli_playgroundd.py
@@ -5,7 +5,10 @@ import asyncio
 
 from typing import Any, List
 import subprocess
+import shutil
 import time
+
+from overlayImageRenderer import OverlayImageRenderer
 
 import os
 
@@ -13,6 +16,13 @@ from pydantic import BaseSettings
 
 # load secrets (phone number)
 secret = Path('secret').read_text()
+
+# load phonebook
+phonebook_dict = json.loads(Path('phonebook').read_text())
+
+# f
+font_file = Path('../overlayImageRenderer/segoeui.ttf')
+assert font_file.exists()
 
 
 def convert_epoch_timestamp_into_str(epoch_timestamp):
@@ -26,6 +36,7 @@ class Settings(BaseSettings):
     signal_upload_path: str = f"{signal_config_path}/uploads/"
     signal_attachments_path: str = f"{signal_config_path}/attachments/"
     signal_cli_executable: str = os.environ.get('SIGNAL_EXEC_PATH', os.path.expanduser('~/signal-cli_built/bin/signal-cli'))
+    signal_cli_phonebook: dict = phonebook_dict
     assert Path(signal_cli_executable).exists()
     assert Path(signal_config_path).exists()
     signal_number: str = secret
@@ -57,22 +68,49 @@ def get_messages(number: str) -> Any:
 json_data_list = get_messages(settings.signal_number)
 print(json_data_list)
 
-db = TinyDB('database.json')
-for envelope_dict in json_data_list:
-    envelope = envelope_dict['envelope']
-    sender = envelope['source']
-    timestamp = envelope['timestamp']
-    timestamp_str = convert_epoch_timestamp_into_str(timestamp)
+if json_data_list != []:
 
-    attachment = envelope_dict['envelope']['dataMessage']['attachments']
+    db = TinyDB('database.json')
+    for envelope_dict in json_data_list:
+        envelope = envelope_dict['envelope']
+        sender = envelope['source']
+        timestamp = envelope['timestamp']
+        timestamp_str = convert_epoch_timestamp_into_str(timestamp)
 
-    attachment_file_str_list = []
-    attachment_path_str_list = []
-    if attachment:
-        for attachment_item in attachment:
-            attachment_path = Path(settings.signal_attachments_path, attachment_item['id'])
-            if attachment_path.exists():
-                attachment_path_str_list.append(attachment_path)
-    db.insert({'timestamp': envelope_dict['envelope']['timestamp'], 'timestamp_str': timestamp_str, 'sender': sender,
-               'has_attachment': bool(attachment), 'attachment_file_str_list': attachment_file_str_list,
-               'envelope': envelope_dict['envelope']})
+        attachment = envelope_dict['envelope']['dataMessage']['attachments']
+
+        attachment_path_str_list = []
+        if attachment:
+            for attachment_item in attachment:
+                attachment_path = Path(settings.signal_attachments_path, attachment_item['id'])
+                if attachment_path.exists():
+                    attachment_path_str_list.append((os.path.splitext(attachment_item['filename'])[1], attachment_path.as_posix()))
+        db.insert({'timestamp': envelope_dict['envelope']['timestamp'], 'timestamp_str': timestamp_str, 'sender': sender,
+                   'has_attachment': bool(attachment), 'attachment_path_str_list': attachment_path_str_list,
+                   'envelope': envelope_dict['envelope']})
+
+
+    Message = Query()
+    messages_with_attachment_list = db.search(Message.has_attachment == True)
+    messages_with_attachment_list.sort(key=lambda k: k['timestamp'])
+    last_message_with_attachment = messages_with_attachment_list[-1]
+
+    sender = last_message_with_attachment['sender']
+    text = last_message_with_attachment['envelope']['dataMessage']['message']
+    attachment_ext, attachment_path = last_message_with_attachment['attachment_path_str_list'][0]
+
+    allowed_extensions_list = ['.jpg', '.jpeg', '.png', '.svg', '.tif']
+
+    if attachment_ext in allowed_extensions_list and sender in phonebook_dict.keys():
+        sender_name = phonebook_dict[sender]
+
+        full_text = f'{sender_name}: {text}'
+
+        background_file_path = os.path.expanduser('~/background'+attachment_ext)
+
+        shutil.copyfile(attachment_path, background_file_path)
+
+        OverlayImageRenderer.overlay_text(background_image_filename=background_file_path,
+                                          overlay_text_string=full_text,
+                                          output_file=background_file_path, font_file=font_file.as_posix())
+
