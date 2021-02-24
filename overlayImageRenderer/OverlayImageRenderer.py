2f4ef968fb17278be014ee6cf419b01321e94384
christian <123>
2021-02-24 19:49:48 +0100

    working first draft

--- overlayImageRenderer/OverlayImageRenderer.py
+++ overlayImageRenderer/OverlayImageRenderer.py
@@ -5,13 +5,16 @@ __version__ = "0.0.1"
 __status__ = "Prototype"
 __name__ = "OverlayImageRenderer"
 
+
+
+
 # import numpy as np
 # import PIL
 import PIL.Image as Image
-# import PIL.ImageDraw as ImageDraw
-# import PIL.ImageFont as ImageFont
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
 import os
-
+from pathlib import Path
 import logging
 
 from subprocess import call
@@ -27,42 +30,46 @@ def datetime_string():
     return dt_string
 
 
-def overlay_text(background_image_filename, overlay_text_string, output_file, overlay_image_filename=None,
+def overlay_text(background_image_filename, overlay_text_string, output_file, font_file, overlay_image_filename=None,
                  end_height=1080, end_width=1920, horizontal_centering=True):
     assert isinstance(end_width, int) and isinstance(end_height, int)
     assert isinstance(overlay_text_string, str)
     logger.info(f'background_image_filename: {background_image_filename}')
     assert os.path.exists(background_image_filename)
-    font_fname = os.getenv('FONT_FILE')
+    font_fname = font_file
 
     background_image = Image.open(background_image_filename)
     w, h = background_image.size
     logger.info("width: {0}px, height: {1}px of uploaded picture".format(str(w), str(h)))
     font_size = round(min(w / 20, h / 20))
     logger.info("font size: " + str(font_size))
-    # draw = ImageDraw.Draw(background_image)
-    # font = ImageFont.truetype(font_fname, font_size)
-    # text_color = "rgb(0, 0, 0)"
-    # shadow_color = "rgb(255, 255, 255)"
+    draw = ImageDraw.Draw(background_image)
+    font = ImageFont.truetype(font_fname, font_size)
+    text_color = "rgb(0, 0, 0)"
+    shadow_color = "rgb(255, 255, 255)"
     draw_x = round(w / 80)
     logger.info("draw_x: " + str(draw_x))
     draw_y = round(h - 1.5 * font_size)
     logger.info("draw_y: " + str(draw_y))
     logger.info("text position: {0}px, {1}px".format(draw_x, draw_y))
 
-    # draw.text((draw_x-3, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x+3, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y-3), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y+3), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x-2, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x+2, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y-2), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y+2), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x-1, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x+1, draw_y), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y-1), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y+1), overlay_text_string, font=font, fill=shadow_color)
-    # draw.text((draw_x, draw_y), overlay_text_string, font=font, fill=text_color)
+    draw.text((draw_x-3, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x+3, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y-3), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y+3), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x-2, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x+2, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y-2), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y+2), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x-1, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x+1, draw_y), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y-1), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y+1), overlay_text_string, font=font, fill=shadow_color)
+    draw.text((draw_x, draw_y), overlay_text_string, font=font, fill=text_color)
+
+    background_image.save(output_file)
+
+    return
 
     pass
 
